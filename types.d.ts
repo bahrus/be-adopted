bc50c459218a2a88dd57c9caa81e9445e4966295
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-05-11 06:13:32 -0400

    Initial commit

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+import {BeDecoratedProps} from 'be-decorated/types';
+import {BeLoadedVirtualProps} from 'be-loaded/types';
+
+export interface BeAdoptedVirtualProps extends BeLoadedVirtualProps{}
+
+export interface BeAdoptedProps extends BeAdoptedVirtualProps{
+    proxy: HTMLStyleElement & BeAdoptedVirtualProps;
+}
+
+export interface BeAdoptedActions{
+    intro(proxy: HTMLStyleElement & BeLoadedVirtualProps, target: HTMLStyleElement, beDecor: BeDecoratedProps): void;
+    
+}
